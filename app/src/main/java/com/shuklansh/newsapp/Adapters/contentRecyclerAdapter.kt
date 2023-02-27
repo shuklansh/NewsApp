13439b63863728e231641b65c04ed0296793b071
Ansh Shukla <ansh.shukla270501@gmail.com>
2023-02-28 00:30:32 +0530

    News home page. category : General

--- app/src/main/java/com/shuklansh/newsapp/Adapters/contentRecyclerAdapter.kt
+++ app/src/main/java/com/shuklansh/newsapp/Adapters/contentRecyclerAdapter.kt
@@ -1,4 +1,39 @@
 package com.shuklansh.newsapp.Adapters
 
-class contentRecyclerAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.shuklansh.newsapp.Models.ArticleModel
+import com.shuklansh.newsapp.R
+import com.squareup.picasso.Picasso
+
+class contentRecyclerAdapter(val context: Context , val ArticleDetails : ArrayList<ArticleModel>) : RecyclerView.Adapter<contentRecyclerAdapter.contentRecyclerViewHolder>(){
+    class contentRecyclerViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val titleText : TextView = itemView.findViewById(R.id.textTitle)
+        val authorText : TextView = itemView.findViewById(R.id.textSource)
+        val imgViewArticle : ImageView = itemView.findViewById(R.id.imgHeadline)
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): contentRecyclerViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.headline_list_item,parent,false)
+        return contentRecyclerViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: contentRecyclerViewHolder, position: Int) {
+        val data = ArticleDetails[position]
+        holder.authorText.text = data.source.getString("name")
+        holder.titleText.text = data.title
+        Picasso.get().load(data.image).into(holder.imgViewArticle)
+
+    }
+
+    override fun getItemCount(): Int {
+       return ArticleDetails.size
+    }
 }
